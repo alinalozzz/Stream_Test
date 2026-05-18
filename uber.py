3bbfdd53be54f659fb04df43e7e8f67690393ca4
Alina Lozano <2024068@student.cct.ie>
2026-05-18 12:27:47 +0100

    Update uber.py

--- uber.py
+++ uber.py
@@ -10,11 +10,11 @@ DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
             'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
 def load_data(nrows):
-	data = pd.read_csv(DATA_URL, nrows=nrows)
-	lowercase = lambda x: str(x).lower()
-	data.rename(lowercase, axis='columns', inplace=True)
-	data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-	return data
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
 
 data_load_state=st.text('Loading Data...')
 load_data(10000)
